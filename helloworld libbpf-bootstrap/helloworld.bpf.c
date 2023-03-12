a10b63f1d0a374c27614d5ae15b5f1f2ddcf5c4d
Matteo-Locatelli <m.locatelli79@studenti.unibg.it>
2023-03-12 20:33:50 +0100

    helloworld libbpf-bootstrap example

---
+++ helloworld libbpf-bootstrap/helloworld.bpf.c
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause
+/* Copyright (c) 2020 Facebook */
+
+// helloworld.bpf.c 
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+
+int bpf_prog(void *ctx) {
+  char msg[] = "Hello, World!";
+  bpf_printk("invoke bpf_prog: %s\n", msg);
+  return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
