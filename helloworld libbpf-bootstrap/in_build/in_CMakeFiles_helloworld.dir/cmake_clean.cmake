a6ae6ccc5a9db05cf386d25d23a51edb155fb3e0
Matteo-Locatelli <m.locatelli79@studenti.unibg.it>
2023-03-17 12:04:26 +0100

    Update helloworld

---
+++ helloworld libbpf-bootstrap/in_build/in_CMakeFiles_helloworld.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld.dir/helloworld.c.o"
+  "CMakeFiles/helloworld.dir/helloworld.c.o.d"
+  "helloworld"
+  "helloworld.bpf.o"
+  "helloworld.pdb"
+  "helloworld.skel.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/helloworld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
